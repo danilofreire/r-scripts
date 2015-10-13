167e9093ff8fc860ccd69e64ef824727719cd176
Danilo Freire <danilofreire@users.noreply.github.com>
2015-10-13 20:01:13 +0100

    Add another example

--- differences-in-differences.R
+++ differences-in-differences.R
@@ -21,10 +21,10 @@ eitc$post93 <- as.numeric(eitc$year >= 1994)
 eitc$anykids <- as.numeric(eitc$children >= 1)
 
 # Compute the four data points needed in the DID calculation:
-a <- sapply(subset(eitc, post93 == 0 & anykids == 0, select=work), mean)
-b <- sapply(subset(eitc, post93 == 0 & anykids == 1, select=work), mean)
-c <- sapply(subset(eitc, post93 == 1 & anykids == 0, select=work), mean)
-d <- sapply(subset(eitc, post93 == 1 & anykids == 1, select=work), mean)
+a <- sapply(subset(eitc, post93 == 0 & anykids == 0, select = work), mean)
+b <- sapply(subset(eitc, post93 == 0 & anykids == 1, select = work), mean)
+c <- sapply(subset(eitc, post93 == 1 & anykids == 0, select = work), mean)
+d <- sapply(subset(eitc, post93 == 1 & anykids == 1, select = work), mean)
 
 # Compute the effect of the EITC on the employment of women with children:
 (d-c)-(b-a)
@@ -33,3 +33,27 @@ d <- sapply(subset(eitc, post93 == 1 & anykids == 1, select=work), mean)
 eitc$p93kids.interaction <- eitc$post93*eitc$anykids
 reg1 <- lm(work ~ post93 + anykids + p93kids.interaction, data = eitc)
 summary(reg1)
+
+# Another example. Here unemployment is the dependent variable and
+# we assume that Austria received the treatment in 1980
+library(Zelig)
+data(macro)
+macro <- subset(macro, country == c("Austria", "Belgium"))
+
+# Create a dummy for treatment
+macro$year1980 <- as.numeric(macro$year >= 1980)
+macro$austria <- as.numeric(macro$country == "Austria")
+
+# Compute the four data points:
+a1 <- sapply(subset(macro, year1980 == 0 & austria == 0, select = unem), mean)
+b1 <- sapply(subset(macro, year1980 == 0 & austria == 1, select = unem), mean)
+c1 <- sapply(subset(macro, year1980 == 1 & austria == 0, select = unem), mean)
+d1 <- sapply(subset(macro, year1980 == 1 & austria == 1, select = unem), mean)
+
+# Compute differences-in-differences
+(d1-c1)-(b1-a1)        
+
+# Regression
+macro$austria.year1980 <- macro$year1980*macro$austria
+reg2 <- lm(unem ~ austria + year1980 + austria.year1980, data = macro)
+summary(reg2)
